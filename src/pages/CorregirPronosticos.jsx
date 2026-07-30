82b0f550612110047a33bbf4215c1f647a011edf
urierofc85 <urielosornio@hotmail.com>
2026-07-30 10:14:10 -0600

    prub

--- src/pages/CorregirPronosticos.jsx
+++ src/pages/CorregirPronosticos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./services/supabase";
+import { supabase } from "../services/supabase";
 
 export default function CorregirPronosticos() {
   const [jornadas, setJornadas] = useState([]);
